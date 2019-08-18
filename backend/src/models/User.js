8d75d2bbc412be657a0a341b0a39c4699bb03ccd
AbnerErnaniADSFatec <abiner.anjos@gmail.com>
2019-08-18 10:09:34 -0300

    add-user-model

---
+++ backend/src/models/User.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    senha: {
+        type: String,
+        required: true
+    },
+    bio: {
+        type: String,
+        required: true
+    },
+    likes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Dev'
+    }],
+    dislikes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Dev'
+    }]
+});
+
+module.exports = model('User', UserSchema);
