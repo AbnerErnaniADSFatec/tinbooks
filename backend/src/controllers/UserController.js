7ddfa6a9f666c30ee6b84975496fa9e239c03ab8
AbnerErnaniADSFatec <abiner.anjos@gmail.com>
2019-08-27 16:42:58 -0300

    fixed-mongo-db-connection

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -2,16 +2,16 @@ const axios = require('axios');
 const User = require('../models/User');
 module.exports = {
     async store(req, res) {
-        const { name, email: emailUser, password, bio, location } = req.body;
-        const userExist = await User.findOne({ email: emailUser });
+        const { name, email, password, bio, location } = req.body;
+        const userExist = await User.findOne({ email: email });
         if (userExist) { return res.json(userExist); }
-        const User = await User.create({
+        const user = await User.create({
             name,
-            emailUser,
+            email,
             password,
             bio,
             location
         });
-        return res.json(User);
+        return res.json(user);
     }
 };
